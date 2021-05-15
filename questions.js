f13397cb9367f30934738519474da35de9a7b461
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-15 19:06:16 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+//Array of objects
+const quiz = [
+    {
+        q:"Surely you're not counting on him for help. He's totally ...............    .",
+        options:["unreliable", "uncountable", "untidy", "dependable", "affectionate"],
+        answer: 0
+    },
+    {
+        q:"A cardiologist tried all he could to reduce the patient's ............... pressure.",
+        options:["cell", "blood", "brain", "tissue", "bone"],
+        answer: 1
+    },
+    {
+        q:"............... of Black Death were not infrequent in the Middle Ages.",
+        options:["Illnesses", "Diseases", "Epidemics", "Paramedics", "Operations"],
+        answer: 2
+    },
+    {
+        q:"We were caught in the ...............-hour trafic.",
+        options:["rush", "bush", "push", "hurry", "emergency"],
+        answer: 0
+    },
+    {
+        q:"He was ............... to the hospital with acute appendicitis.",
+        options:["crashed", "reacted", "pressurized", "activated", "rushed"],
+        answer: 4
+    },
+    {
+        q:"I do not wish to ............... these results at this stage. You must give me a little bit more information about the research techniques you used in the project.",
+        options:["interpret", "regulate", "disallow", "prove", "disapprove"],
+        answer: 0
+    },
+    {
+     q:"We do not approve of research projects that ............... vivisection of animals.",
+        options:["suspect", "involve", "emerge", "disprove", "unite"],
+        answer: 1   
+    },
+    {
+        q:"Your latest report, I'm ..............., is not up to your usual standard.",
+        options:["happy to say", "jealous and envious", "afraid", "frightened", "frightening"],
+        answer: 2
+    },
+    {
+        q:"He is a greedy, dishonest person. Watch out for his ............... tricks.",
+        options:["delightful", "incorrect", "charming", "dirty", "faithful"],
+        answer: 3
+    },
+    {
+        q:"It is the ............... that counts, not the thought.",
+        options:["feed", "seed", "weed", "need", "deed"],
+        answer: 4
+    }
+]
